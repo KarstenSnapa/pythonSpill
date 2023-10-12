961dd0e66a630b2b320d3d5c0d70e35fec6566cc
KarstenSnapa <karsten.snapa@gmail.com>
2023-10-12 13:45:55 +0200

    Framework
    Rammeverket Matheo satte opp

---
+++ Spill.py
@@ -0,0 +1,24 @@
+import pygame, sys
+
+
+
+#display
+
+
+#Funksjoner
+
+def Update():
+    
+
+Running = True
+
+while Running:
+    for evnt in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Running = False
+
+    
+
+pygame.QUIT()
+sys.exit()
+
