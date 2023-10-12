3023422fa67f282bc15a448fa546849507901f7d
KarstenSnapa <karsten.snapa@gmail.com>
2023-10-12 13:48:52 +0200

    Framework with ChatGPT
    Framework built by CHATGPT

--- Spill.py
+++ Spill.py
@@ -1,24 +1,35 @@
-import pygame, sys
+import pygame
+import sys
 
+# Initialize Pygame
+pygame.init()
 
+# Display setup
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pygame Template")
 
-#display
-
-
-#Funksjoner
-
-def Update():
-    
-
-Running = True
-
-while Running:
-    for evnt in pygame.event.get():
+# Main loop
+running = True
+while running:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            Running = False
+            running = False
 
-    
+    # Update game logic here
+    # ...
 
-pygame.QUIT()
+    # Clear the screen (optional)
+    screen.fill((0, 0, 0))  # Fill the screen with a black color
+
+    # Draw game elements here
+    # ...
+
+    # Update the display
+    pygame.display.flip()
+
+# Quit Pygame and exit the program
+pygame.quit()
 sys.exit()
 
