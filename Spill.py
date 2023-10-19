2281dc667df1657035ac3328a42bcd49480c333d
KarstenSnapa <karsten.snapa@gmail.com>
2023-10-19 13:11:01 +0200

    Movement + bullets
    lagd firkant som kan bevege seg, og definert bullet.

--- Spill.py
+++ Spill.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 pygame.init()
 
 display = pygame.display.set_mode((800, 600))
@@ -16,6 +17,14 @@ class Player:
         pygame.draw.rect(display, (255, 5, 5), (self.x, self.y, self.width, self.height))
 
 
+class PlayerBullet:
+    def __init__(self, x, y, mouse_x, mouse_y):
+        self.x = x
+        self.y = y
+        self.mouse_x = mouse_x
+        self.mouse_y = mouse_y
+
+
 player = Player(400, 300, 32, 32)
 
 display_scroll = [0, 0]
@@ -27,10 +36,23 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
             pygame.QUIT
+
+    pygame.draw.rect(display, (255, 0, 0), (100-display_scroll[0], 100-display_scroll[1], 16, 16) )
     
     keys = pygame.key.get_pressed()
 
+    if keys[pygame.K_d]:
+        display_scroll[0] -= 5
     
+    if keys[pygame.K_a]:
+        display_scroll[0] += 5
+
+    if keys[pygame.K_s]:
+        display_scroll[1] -= 5
+
+    if keys[pygame.K_w]:
+        display_scroll[1] += 5
+
 
     player.main(display)
 
